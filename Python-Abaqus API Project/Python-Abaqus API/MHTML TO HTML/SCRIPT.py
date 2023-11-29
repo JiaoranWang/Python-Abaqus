56b732988a79f1a9d08c60458022b08791ad669c
AmandaWang_HP <AmandaWang0125@163.com>
2023-11-29 22:46:00 +0800

    ~hp_update23

--- Python-Abaqus API Project/Python-Abaqus API/MHTML TO HTML/SCRIPT.py
+++
@@ -1,9 +0,0 @@
-import jpype
-import asposecells
-
-jpype.startJVM()
-from asposecells.api import Workbook
-
-workbook = Workbook("Abaqus-Python API Operation Manual.mht");
-workbook.Save("Abaqus-Python API Operation Manual.html");
-jpype.shutdownJVM()
