41ef2a71bd7b53652adfa18a653fa65534df7938
JiaoranWang <AmandaWang0125@163.com>
2023-11-29 10:41:38 +0800

    SCRIPT.PY change

--- [Python-Abaqus API Project]/Python-Abaqus API/MHTML TO HTML/SCRIPT.py
+++ [Python-Abaqus API Project]/Python-Abaqus API/MHTML TO HTML/SCRIPT.py
@@ -3,7 +3,7 @@ import asposecells
 
 jpype.startJVM()
 from asposecells.api import Workbook
-
+### To change the .mht => .html
 workbook = Workbook("Abaqus-Python API Operation Manual.mht");
 workbook.Save("Abaqus-Python API Operation Manual.html");
 jpype.shutdownJVM()
