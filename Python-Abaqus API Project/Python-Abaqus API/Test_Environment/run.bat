c28f0c7eb5796e4491abc86dda25405eb6ace9e2
JiaoranWang <AmandaWang0125@163.com>
2023-12-02 11:05:05 +0800

    LENOVO_UPDATING_231202

--- Python-Abaqus API Project/Python-Abaqus API/Test_Environment/run.bat
+++
@@ -1,6 +0,0 @@
-cd C:\Users\jiaor\PycharmProjects\pythonProject_HP\Python-Abaqus API Project\Python-Abaqus API\Test_Environment
-dir
-start "Abaqus_Command.lnk"
-
-### exit:
-exit###
--- Python-Abaqus API Project/Python-Abaqus API/Test_Environment/runcae.bat
+++
@@ -1,7 +0,0 @@
-cd ..
-cd C:\Users\jiaor\PycharmProjects\pythonProject_HP\Python-Abaqus API Project\Python-Abaqus API\Exe_Scripts
-cd Exe_Scripts
-dir
-abaqus cae script=Exe_1.py
-//
-abaqus cae script=test.py
